99e4436ec9685c01c1cb0c5486845066516f1292
ucheobi <ucheobi2009@yahoo.com>
2025-07-13 22:55:44 +0200

    Task_3: implement get book based on author

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -29,8 +29,13 @@ public_users.get('/isbn/:isbn',function (req, res) {
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const author = req.params.author;
+  const booksByAuthor = Object.values(books).filter(book => book.author.toLowerCase() === author.toLowerCase());
+
+  if (booksByAuthor.length > 0) {
+    return res.status(200).json(booksByAuthor);   
+  }
+  return res.status(404).json({message: "No books found for this author"});
 });
 
 // Get all books based on title
